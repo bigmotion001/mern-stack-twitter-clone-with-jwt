ec61e346e22c06a6f660c6fac4223d9087956040
Obasi <bigmotion001@gmail.com>
2025-03-23 13:29:22 -0700

    refactor server setup for improved path handling and static file serving; update package.json for local dependency

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-import express from "express"
+import path from "path";
+import express from "express";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -10,26 +11,18 @@ import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { v2 as cloudinary } from "cloudinary";
-import{app, server} from "./socket/socket.js";
-import path from "path";
+import { app, server } from "./socket/socket.js";
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_USERNAME ,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET,
+  cloud_name: process.env.CLOUDINARY_USERNAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
-
-
-
 const PORT = process.env.PORT;
-app.use(cors({origin:"http://localhost:5173", credentials:true}))
-
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 const __dirname = path.resolve();
-
-
-
 
 //middelware
 app.use(cookieParser());
@@ -42,22 +35,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notification", notificationRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-
-
-
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-    app.get("*", (req, res)=>{
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
-  }
-
-
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
-    connectDB();
-    console.log(`Server started on PORT: ${PORT}`);
+  connectDB();
+  console.log(`Server started on PORT: ${PORT}`);
 });
